c9e630b9884ebac925e758aa402cb74c5150a87b
Pietro Caproni <pietrodipaulo4@gmail.com>
2021-05-12 20:50:41 -0300

    annotation autowired e regra de negócio

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,24 +1,28 @@
 
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.repository.UserRepository;
+//import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
 
 @Controller
 public class UserController {
 
-	private UserRepository userRepository;
+	//definição automatica do spring
+	@Autowired
+	private UserService userService;
 	
-	public UserController(UserRepository userRepository) {
+	/*public UserController(UserRepository userRepository) {
 		this.userRepository = userRepository;
-	}
+	}*/
 	
 	@RequestMapping("/user")
 	public String getUsers(Model model) {
-		model.addAttribute("usersList", this.userRepository.findAll());
+		model.addAttribute("usersList", this.userService.findAll());
 		
 		return "user";
 		
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.demo.entity.User;
 // habilita o uso do java repository em outras classes
 public interface UserRepository extends JpaRepository<User, Long> {
-	//@Query("SELECT * FROM User where name like %?1%"  )
+ //@Query("SELECT * FROM User where name like %?1%"  )
  //User findByNameQualquerCoisa(String name);
  List<User> findByName(String name);
 }
